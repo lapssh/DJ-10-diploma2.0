0a36063d6f4a9ba4e2cb18508de56eecdf0280d4
lapssh <lapssh@yandex.ru>
2020-08-18 10:28:51 +0300

    отформатирован urls.py - все urlspatterns собраны в один - static вынесен в конец

--- tyndex/tyndex/urls.py
+++ tyndex/tyndex/urls.py
@@ -7,27 +7,17 @@ from market.views import index, view_all_articles, one_article, product_view, pr
     add_to_cart
 
 urlpatterns = [
-    path('index.html', index, name='index'),
-    path('', index),
-    path('market/', include('market.urls')),
-    path('admin/', admin.site.urls),
-]
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
-
-urlpatterns += [
-    path('articles/read/<str:name>', one_article, name='one_article'),
-    path('articles/', view_all_articles, name='view_all_articles'),
-]
-urlpatterns += [
-                   path('product_list_view', product_list_view, name='product_list_view'),
-                   path('<str:section_slug>/<str:category_slug>/<str:slug>', product_view, name='product'),
-                   path('<str:section_slug>/<str:category_slug>', product_list_view, name='products'),
-                   path('', product_list_view, name='products'),
-               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [
-    path('cart/', show_cart_view, name='show_cart'),
-    path('add_to_cart/', add_to_cart, name='add_to_cart'),
-]
+                  path('index.html', index, name='index'),
+                  path('', index),
+                  path('market/', include('market.urls')),
+                  path('admin/', admin.site.urls),
+                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('articles/read/<str:name>', one_article, name='one_article'),
+                  path('articles/', view_all_articles, name='view_all_articles'),
+                  path('product_list_view', product_list_view, name='product_list_view'),
+                  path('<str:section_slug>/<str:category_slug>/<str:slug>', product_view, name='product'),
+                  path('<str:section_slug>/<str:category_slug>', product_list_view, name='products'),
+                  path('', product_list_view, name='products'),
+                  path('cart/', show_cart_view, name='show_cart'),
+                  path('add_to_cart/', add_to_cart, name='add_to_cart'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
